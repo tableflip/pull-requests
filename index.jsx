4bc70807e8e45adad6d781b87b7e670b85fee4f7
Richard Silverton <silverton.richard@googlemail.com>
2016-05-24 17:12:24 +0100

    Adds login with github

--- index.jsx
+++ index.jsx
@@ -1,12 +1,33 @@
 import express from 'express'
 import config from 'config'
+import githubOauth from 'github-oauth'
 import root from './root'
 
 const app = express()
 app.use(express.static('public'))
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.send(root())
+})
+app.get('/login-error', (req, res) => {
+  res.send('There was a login error')
+})
+
+const github = githubOauth({
+  githubClient: config.github.client,
+  githubSecret: config.github.secret,
+  baseURL: config.rootUrl,
+  loginURI: '/login',
+  callbackURI: '/callback',
+  scope: 'user'
+})
+github.addRoutes(app, (err, token, res) => {
+  if (err) {
+    console.error(err)
+    res.redirect('/login-error')
+  }
+  console.log(token)
+  res.redirect('/')
 })
 
 const server = app.listen(config.port, () => {
